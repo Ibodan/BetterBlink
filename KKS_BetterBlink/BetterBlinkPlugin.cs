93e415f2f404482f5541ce1fa56da47cf2d32c70
Ibodan <Ibodan@gmail.com>
2024-12-23 02:19:02 +0900

    Fix indent character of source code for nice look on github

--- KKS_BetterBlink/BetterBlinkPlugin.cs
+++ KKS_BetterBlink/BetterBlinkPlugin.cs
@@ -4,8 +4,8 @@ namespace KK_BetterBlink
 {
     [BepInProcess("KoikatsuSunshine")]
     [BepInProcess("KoikatsuSunshine_VR")]
-	[BepInProcess("CharaStudio")]
-	public partial class BetterBlinkPlugin : BaseUnityPlugin
+    [BepInProcess("CharaStudio")]
+    public partial class BetterBlinkPlugin : BaseUnityPlugin
     {
         public const string Name = "KKS_BetterBlink";
         public const string GUID = "KKS_BetterBlink";
--- KK_BetterBlink/BetterBlinkPlugin.cs
+++ KK_BetterBlink/BetterBlinkPlugin.cs
@@ -2,12 +2,12 @@
 
 namespace KK_BetterBlink
 {
-	[BepInProcess("Koikatu")]
-	[BepInProcess("Koikatsu Party")]
-	[BepInProcess("KoikatuVR")]
-	[BepInProcess("Koikatsu Party VR")]
-	[BepInProcess("CharaStudio")]
-	public partial class BetterBlinkPlugin : BaseUnityPlugin
+    [BepInProcess("Koikatu")]
+    [BepInProcess("Koikatsu Party")]
+    [BepInProcess("KoikatuVR")]
+    [BepInProcess("Koikatsu Party VR")]
+    [BepInProcess("CharaStudio")]
+    public partial class BetterBlinkPlugin : BaseUnityPlugin
     {
         public const string Name = "KK_BetterBlink";
         public const string GUID = "KK_BetterBlink";
